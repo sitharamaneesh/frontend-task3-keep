a298277d630fbeb3528dc959d85c1a218a34855c
Sithara Maneesh <sitharasarath03@gmail.com>
2021-11-21 23:02:01 +0530

    made textbox interactive

--- task3.js
+++ task3.js
@@ -21,3 +21,42 @@ function out()
     var obj=document.getElementById("note-list")
     obj.setAttribute("style","background-color:tranparent;")
 }
+
+
+function newlist(){
+
+    var newlist_button=document.getElementById('newlist_id')
+    newlist_button.setAttribute("style","opacity: 0.87;background-color: rgba(95,99,104,0.157);")
+
+}
+
+function out_newlist()
+{
+    var newlist_button=document.getElementById('newlist_id')
+    newlist_button.setAttribute("style","opacity: 0.87;background-color: transparent")
+}
+
+function enter_note()
+{
+
+    var list=document.getElementById('newlist_id');
+    list.setAttribute("style","display:none;")
+
+    var title=document.getElementById('title_div');
+    title.innerHTML="Title...";
+    title.setAttribute("style","flex:1;width:100%;")
+     
+    var note=document.getElementById('note_div');
+    note.setAttribute("style","display:block;min-height:150px;")
+
+    
+}
+
+function leave_note()
+{
+    var title=document.getElementById('title_div');
+    title.innerHTML="Take a note..."; 
+
+    var note=document.getElementById('note_div');
+    note.setAttribute("style","display:none;")
+}
